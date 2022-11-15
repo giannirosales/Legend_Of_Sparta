1d14687d711392c4fd2083b9f7cafccb5587cede
Gianni Rosales <gianni.rosales@icloud.com>
2022-11-14 19:30:40 -0800

    Version 1.0

--- src/main/java/com/example/legend_of_sparta/SpartanController.java
+++ src/main/java/com/example/legend_of_sparta/SpartanController.java
@@ -3,7 +3,7 @@ package com.example.legend_of_sparta;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class SpartanController {
     @FXML
     private Label welcomeText;
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module com.example.legend_of_sparta {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
+
     opens com.example.legend_of_sparta to javafx.fxml;
     exports com.example.legend_of_sparta;
 }
